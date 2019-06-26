096e981ade7478f87b8290d27c2a322317874822
Muchsin Alkatiri <muchsinalkatiri36@yahoo.com>
2019-06-26 21:11:22 +0700

    file ktglan

--- testlur.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect = new mysqli('localhost', 'root', '', 'ta_toeic');
-
-$cur = $_GET['cur'];
-
-$query = mysqli_query($connect, "UPDATE data_ujian SET audio_curent_time = $cur WHERE id_data_ujian = 97");
